9b469de077d2a48091cc861be4283d438897c03d
Tres Trantham <tres@trestrantham.com>
2020-01-12 22:37:10 -0500

    Initial app setup

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+// tslint:disable-next-line:no-var-requires
+require("dotenv").config();
+
+const { QUANTCONNECT_USER_ID, QUANTCONNECT_TOKEN, QUANTCONNECT_PROJECT_ID } = process.env;
+
+(async () => {
+  // tslint:disable-next-line:no-console
+  console.log("STARTED");
+})();
